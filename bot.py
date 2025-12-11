d2cac75152fe3882507cf41830c63fb406976f4f
LucasLin68 <lucaslin686868@gmail.com>
2025-12-11 19:03:52 +0800

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 import os
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 token = os.getenv('TOKEN')
@@ -11,6 +13,17 @@ intents.message_content = True
 intents.guild_messages = True
 
 bot = commands.Bot(command_prefix = '!', intents = intents)
+
+async def load_extensions():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+@bot.command()
+async def sync(ctx):
+    bot.tree.copy_global_to(guild = ctx.guild)
+    synced = await bot.tree.sync(guild = ctx.guild)
+    await ctx.send("synced")
 
 @bot.event
 async def on_ready():
@@ -29,5 +42,13 @@ async def on_message(message):
 @bot.command()
 async def hello(ctx):
   await ctx.send(f"你好啊{ctx.author.mention}")
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(token)
+
+if __name__ == '__main__':
+    asyncio.run(main())
 
 bot.run(token)
