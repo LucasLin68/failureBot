b27fb046689d77e725ad54fde268bd21108a4651
LucasLin68 <lucaslin686868@gmail.com>
2025-12-18 18:57:21 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ from discord import app_commands
 import os
 from dotenv import load_dotenv
 import asyncio
+from flask import Flask
+from threading import Thread
+
 
 load_dotenv()
 token = os.getenv('TOKEN')
@@ -17,6 +20,7 @@ bot = commands.Bot(command_prefix = '!', intents = intents)
 async def load_extensions():
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
+            print(filename)
             await bot.load_extension(f'cogs.{filename[:-3]}')
 
 @bot.command()
@@ -24,10 +28,6 @@ async def sync(ctx):
     bot.tree.copy_global_to(guild = ctx.guild)
     synced = await bot.tree.sync(guild = ctx.guild)
     await ctx.send("synced")
-
-@bot.event
-async def on_ready():
-    print(f'We have logged in as {bot.user}')
 
 @bot.event
 async def on_message(message):
@@ -45,10 +45,29 @@ async def hello(ctx):
 
 async def main():
     async with bot:
+        await bot.start(token)
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "上線了"
+
+def run_flask():
+  
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+async def main():
+   
+    t = Thread(target=run_flask)
+    t.daemon = True
+    t.start()
+    
+ 
+    async with bot:
         await load_extensions()
         await bot.start(token)
 
 if __name__ == '__main__':
-    asyncio.run(main())
-
-bot.run(token)
+  	asyncio.run(main())
